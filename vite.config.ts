240947abbc2054d6f2d4c0ac41e4fbb0f154a1e1
Nhan Nguyen <153412098+nhannm-vn@users.noreply.github.com>
2025-03-02 18:39:38 +0700

    done fix tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
-
+import tailwindcss from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -9,7 +10,10 @@ export default defineConfig({
     port: 3000
   },
   css: {
-    devSourcemap: true
+    devSourcemap: true,
+    postcss: {
+      plugins: [tailwindcss(), autoprefixer()]
+    }
   },
   resolve: {
     alias: {
