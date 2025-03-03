501d56494c0c1ada7de5682972dc09b696c9b492
Nhan Nguyen <153412098+nhannm-vn@users.noreply.github.com>
2025-03-03 21:24:04 +0700

    update img footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,19 +2,15 @@ function Footer() {
   return (
     <footer className='py-16 bg-neutral-100'>
       <div className='max-w-7xl mx-auto px-4'>
-        <div className='grid grid-cols-1 lg:grid-cols-3 gap-4'>
-          <div className='lg:col-span-1'>
-            <div>© 2025 Shopee. Tất cả các quyền được bảo lưu.</div>
-          </div>
-          <div className='lg:col-span-2'>
-            <div>
-              Quốc gia & Khu vực: Singapore | Indonesia | Thái Lan | Malaysia | Việt Nam | Philippines | Brazil | México
-              Colombia | Chile | Đài Loan
-            </div>
-          </div>
-        </div>
         <div className='text-center text-sm mt-10'>
-          <div>Công ty TNHH Shopee</div>
+          <div className='justify-items-center align-middle my-5'>
+            <img
+              className='block w-5/12'
+              src='https://deo.shopeemobile.com/shopee/shopee-pcmall-live-sg/assets/9765d68a8945750d.png'
+              alt=''
+            />
+          </div>
+          <div className='py-7'>Công ty TNHH Shopee</div>
           <div className='mt-2'>
             Địa chỉ: Tầng 4-5-6, Tòa nhà Capital Place, số 29 đường Liễu Giai, Phường Ngọc Khánh, Quận Ba Đình, Thành
             phố Hà Nội, Việt Nam. Tổng đài hỗ trợ: 19001221 - Email: cskh@hotro.shopee.vn
