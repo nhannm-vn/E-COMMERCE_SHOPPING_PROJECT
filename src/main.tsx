d115d07ce0857c4c00b0b1cad18d638fb6733735
Nhan Nguyen <153412098+nhannm-vn@users.noreply.github.com>
2025-03-03 19:37:43 +0700

    use BrowserRouter and create pakage layouts and pages

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
+import { BrowserRouter } from 'react-router-dom'
+
+//BrowserRouter: bọc app vào để chia route
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 )
