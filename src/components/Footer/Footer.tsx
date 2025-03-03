1a3bae58641d1e0e12f2555f42ff2967ba5f31dd
Nhan Nguyen <153412098+nhannm-vn@users.noreply.github.com>
2025-03-03 21:55:18 +0700

    update design footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,177 @@
+import { Link } from 'react-router-dom'
+
 function Footer() {
   return (
     <footer className='py-16 bg-neutral-100'>
       <div className='max-w-7xl mx-auto px-4'>
+        <div className='flex justify-around'>
+          <div>
+            <p className='font-bold mb-7'>THEO DÕI SHOPEE</p>
+            <Link to=''>
+              <div className='my-2 flex'>
+                <div className='mr-2'>
+                  <img src='https://down-vn.img.susercontent.com/file/2277b37437aa470fd1c71127c6ff8eb5' alt='' />
+                </div>
+                <p>Facebook</p>
+              </div>
+            </Link>
+            <Link to=''>
+              <div className='my-2 flex'>
+                <div className='mr-2'>
+                  <img src='https://down-vn.img.susercontent.com/file/5973ebbc642ceee80a504a81203bfb91' alt='' />
+                </div>
+                <p>Instagram</p>
+              </div>
+            </Link>
+            <Link to=''>
+              <div className='my-2 flex'>
+                <div className='mr-2'>
+                  <img src='https://down-vn.img.susercontent.com/file/f4f86f1119712b553992a75493065d9a' alt='' />
+                </div>
+                <p>LinkedIn</p>
+              </div>
+            </Link>
+          </div>
+          <div>
+            <div>
+              <p className='font-bold mb-7'>THANH TOÁN</p>
+              <div>
+                <div className='mb-2'>
+                  <div className='inline-flex items-center justify-center bg-white border border-gray-300 rounded-lg shadow-md px-4 py-2 mr-2'>
+                    <img
+                      className='h-5'
+                      src='https://down-vn.img.susercontent.com/file/d4bbea4570b93bfd5fc652ca82a262a8'
+                      alt=''
+                    />
+                  </div>
+                  <div className='inline-flex items-center justify-center bg-white border border-gray-300 rounded-lg shadow-md px-4 py-2 mr-2'>
+                    <img
+                      className='h-5'
+                      src='https://down-vn.img.susercontent.com/file/a0a9062ebe19b45c1ae0506f16af5c16'
+                      alt=''
+                    />
+                  </div>
+                  <div className='inline-flex items-center justify-center bg-white border border-gray-300 rounded-lg shadow-md px-4 py-2 mr-2'>
+                    <img
+                      className='h-5'
+                      src='https://down-vn.img.susercontent.com/file/38fd98e55806c3b2e4535c4e4a6c4c08'
+                      alt=''
+                    />
+                  </div>
+                </div>
+                <div className='mb-2'>
+                  <div className='inline-flex items-center justify-center bg-white border border-gray-300 rounded-lg shadow-md px-4 py-2 mr-2'>
+                    <img
+                      className='h-5'
+                      src='https://down-vn.img.susercontent.com/file/bc2a874caeee705449c164be385b796c'
+                      alt=''
+                    />
+                  </div>
+                  <div className='inline-flex items-center justify-center bg-white border border-gray-300 rounded-lg shadow-md px-4 py-2 mr-2'>
+                    <img
+                      className='h-5'
+                      src='https://down-vn.img.susercontent.com/file/2c46b83d84111ddc32cfd3b5995d9281'
+                      alt=''
+                    />
+                  </div>
+                  <div className='inline-flex items-center justify-center bg-white border border-gray-300 rounded-lg shadow-md px-4 py-2 mr-2'>
+                    <img
+                      className='h-5'
+                      src='https://down-vn.img.susercontent.com/file/5e3f0bee86058637ff23cfdf2e14ca09'
+                      alt=''
+                    />
+                  </div>
+                </div>
+                <div className='mb-2'>
+                  <div className='inline-flex items-center justify-center bg-white border border-gray-300 rounded-lg shadow-md px-4 py-2 mr-2'>
+                    <img
+                      className='h-5'
+                      src='https://down-vn.img.susercontent.com/file/9263fa8c83628f5deff55e2a90758b06'
+                      alt=''
+                    />
+                  </div>
+                  <div className='inline-flex items-center justify-center bg-white border border-gray-300 rounded-lg shadow-md px-4 py-2 mr-2'>
+                    <img
+                      className='h-5'
+                      src='https://down-vn.img.susercontent.com/file/0217f1d345587aa0a300e69e2195c492'
+                      alt=''
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div>
+              <p className='font-bold my-7'>THANH TOÁN</p>
+              <div>
+                <div className='mb-2'>
+                  <div className='inline-flex items-center justify-center bg-white border border-gray-300 rounded-lg shadow-md px-4 py-2 mr-2'>
+                    <img
+                      className='h-5'
+                      src='https://down-vn.img.susercontent.com/file/d4bbea4570b93bfd5fc652ca82a262a8'
+                      alt=''
+                    />
+                  </div>
+                  <div className='inline-flex items-center justify-center bg-white border border-gray-300 rounded-lg shadow-md px-4 py-2 mr-2'>
+                    <img
+                      className='h-5'
+                      src='https://down-vn.img.susercontent.com/file/a0a9062ebe19b45c1ae0506f16af5c16'
+                      alt=''
+                    />
+                  </div>
+                  <div className='inline-flex items-center justify-center bg-white border border-gray-300 rounded-lg shadow-md px-4 py-2 mr-2'>
+                    <img
+                      className='h-5'
+                      src='https://down-vn.img.susercontent.com/file/38fd98e55806c3b2e4535c4e4a6c4c08'
+                      alt=''
+                    />
+                  </div>
+                </div>
+                <div className='mb-2'>
+                  <div className='inline-flex items-center justify-center bg-white border border-gray-300 rounded-lg shadow-md px-4 py-2 mr-2'>
+                    <img
+                      className='h-5'
+                      src='https://down-vn.img.susercontent.com/file/bc2a874caeee705449c164be385b796c'
+                      alt=''
+                    />
+                  </div>
+                  <div className='inline-flex items-center justify-center bg-white border border-gray-300 rounded-lg shadow-md px-4 py-2 mr-2'>
+                    <img
+                      className='h-5'
+                      src='https://down-vn.img.susercontent.com/file/2c46b83d84111ddc32cfd3b5995d9281'
+                      alt=''
+                    />
+                  </div>
+                  <div className='inline-flex items-center justify-center bg-white border border-gray-300 rounded-lg shadow-md px-4 py-2 mr-2'>
+                    <img
+                      className='h-5'
+                      src='https://down-vn.img.susercontent.com/file/5e3f0bee86058637ff23cfdf2e14ca09'
+                      alt=''
+                    />
+                  </div>
+                </div>
+                <div className='mb-2'>
+                  <div className='inline-flex items-center justify-center bg-white border border-gray-300 rounded-lg shadow-md px-4 py-2 mr-2'>
+                    <img
+                      className='h-5'
+                      src='https://down-vn.img.susercontent.com/file/9263fa8c83628f5deff55e2a90758b06'
+                      alt=''
+                    />
+                  </div>
+                  <div className='inline-flex items-center justify-center bg-white border border-gray-300 rounded-lg shadow-md px-4 py-2 mr-2'>
+                    <img
+                      className='h-5'
+                      src='https://down-vn.img.susercontent.com/file/0217f1d345587aa0a300e69e2195c492'
+                      alt=''
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div>
+            <p className='font-bold mb-7'>TẢI ỨNG DỤNG SHOPEE</p>
+          </div>
+        </div>
         <div className='text-center text-sm mt-10'>
           <div className='justify-items-center align-middle my-5'>
             <img
