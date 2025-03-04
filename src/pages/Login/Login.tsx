701e878af53ede338c006608ce54afcebd442cde
Nhan Nguyen <153412098+nhannm-vn@users.noreply.github.com>
2025-03-04 07:55:20 +0700

    code input password

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -7,7 +7,7 @@ function Login() {
           {/* form */}
           <div className='lg:col-span-2 lg:col-start-4'>
             <form className='p-10 rounded bg-white shadow-sm'>
-              <div className='text-2xl'>Đăng ký</div>
+              <div className='text-2xl'>Đăng Nhập</div>
               <div className='mt-8'>
                 <input
                   type='email'
@@ -16,7 +16,17 @@ function Login() {
                   placeholder='Email'
                 />
                 {/* min-h-[1rem]: giúp luôn có chiều cao kể cả không có lỗi */}
-                <div className='mt-1 text-red-600 min-h-[1rem] text-sm'>Email không hợp lệ</div>
+                <div className='mt-1 text-red-600 min-h-[1rem] text-sm'></div>
+              </div>
+              <div className='mt-3'>
+                <input
+                  type='password'
+                  name='password'
+                  className='p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm focus:shadow-md'
+                  placeholder='Password'
+                />
+                {/* min-h-[1rem]: giúp luôn có chiều cao kể cả không có lỗi */}
+                <div className='mt-1 text-red-600 min-h-[1rem] text-sm'></div>
               </div>
             </form>
           </div>
