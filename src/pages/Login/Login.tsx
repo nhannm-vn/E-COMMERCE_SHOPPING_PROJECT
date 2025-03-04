5e9197be293a0914adc4a286b22996c918ca64b8
Nhan Nguyen <153412098+nhannm-vn@users.noreply.github.com>
2025-03-04 07:27:09 +0700

    code page login

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,5 +1,19 @@
 function Login() {
-  return <div>Login</div>
+  return (
+    <div className='bg-orange'>
+      <div className='max-w-7xl mx-auto px-4'>
+        <div className='grid grid-cols-1 py-12 lg:grid-cols-5 lg:pr-10 lg:py-32'>
+          {/* màn hình lớn thì chiếm 2 cột, bắt đầu từ cột thứ 4.*/}
+          {/* form */}
+          <div className='lg:col-span-2 lg:col-start-4'>
+            <form className='p-10 rounded bg-white shadow-sm'>
+              <div className='text-2xl'>Đăng ký</div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Login
