420bcd7b291f2c03ed82669c9dba64fe640c13bf
Nhan Nguyen <153412098+nhannm-vn@users.noreply.github.com>
2025-03-04 08:03:13 +0700

    code register

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,5 +1,55 @@
 function Register() {
-  return <div>Register</div>
+  return (
+    <div className='bg-orange'>
+      <div className='max-w-7xl mx-auto px-4'>
+        <div className='grid grid-cols-1 py-12 lg:grid-cols-5 lg:pr-10 lg:py-32'>
+          {/* màn hình lớn thì chiếm 2 cột, bắt đầu từ cột thứ 4.*/}
+          {/* form */}
+          <div className='lg:col-span-2 lg:col-start-4'>
+            <form className='p-10 rounded bg-white shadow-sm'>
+              <div className='text-2xl'>Đăng Ký</div>
+              <div className='mt-8'>
+                <input
+                  type='email'
+                  name='email'
+                  className='p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm focus:shadow-md'
+                  placeholder='Email'
+                />
+                {/* min-h-[1rem]: giúp luôn có chiều cao kể cả không có lỗi */}
+                <div className='mt-1 text-red-600 min-h-[1rem] text-sm'></div>
+              </div>
+              <div className='mt-3'>
+                <input
+                  type='password'
+                  name='password'
+                  className='p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm focus:shadow-md'
+                  placeholder='Password'
+                />
+                {/* min-h-[1rem]: giúp luôn có chiều cao kể cả không có lỗi */}
+                <div className='mt-1 text-red-600 min-h-[1rem] text-sm'></div>
+              </div>
+              <div className='mt-3'>
+                <input
+                  type='password'
+                  name='confirm_password'
+                  className='p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm focus:shadow-md'
+                  placeholder='Confirm Password'
+                />
+                {/* min-h-[1rem]: giúp luôn có chiều cao kể cả không có lỗi */}
+                <div className='mt-1 text-red-600 min-h-[1rem] text-sm'></div>
+              </div>
+              {/* button */}
+              <div className='mt-3'>
+                <button className='w-full text-center py-4 px-2 uppercase bg-red-500 text-white text-sm hover:bg-red-600'>
+                  Đăng Ký
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Register
