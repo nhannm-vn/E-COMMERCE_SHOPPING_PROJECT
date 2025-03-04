389899ba850a237097e2fa59f9f9104b691a4c40
Nhan Nguyen <153412098+nhannm-vn@users.noreply.github.com>
2025-03-04 07:37:32 +0700

    code input login and register

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -8,6 +8,16 @@ function Login() {
           <div className='lg:col-span-2 lg:col-start-4'>
             <form className='p-10 rounded bg-white shadow-sm'>
               <div className='text-2xl'>Đăng ký</div>
+              <div className='mt-8'>
+                <input
+                  type='email'
+                  name='email'
+                  className='p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm focus:shadow-md'
+                  placeholder='Email'
+                />
+                {/* min-h-[1rem]: giúp luôn có chiều cao kể cả không có lỗi */}
+                <div className='mt-1 text-red-600 min-h-[1rem] text-sm'>Email không hợp lệ</div>
+              </div>
             </form>
           </div>
         </div>
