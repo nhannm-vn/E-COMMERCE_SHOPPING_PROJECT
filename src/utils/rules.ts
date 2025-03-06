a60239147c9cdf6a23a0e0f2b8bdc63daf6fcb35
Nhan Nguyen <153412098+nhannm-vn@users.noreply.github.com>
2025-03-06 20:36:12 +0700

    update rule for email

--- src/utils/rules.ts
+++ src/utils/rules.ts
@@ -10,6 +10,14 @@ export const rules = {
       value:
         /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
       message: 'Email không đúng định dạng'
+    },
+    maxLength: {
+      value: 160,
+      message: 'Độ dài từ 5 - 160 ký tự'
+    },
+    minLength: {
+      value: 5,
+      message: 'Độ dài từ 5 - 160 ký tự'
     }
   }
 }
