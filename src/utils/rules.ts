fc1d3247c532abcda04f82c20e16e0aeb9243a09
Nhan Nguyen <153412098+nhannm-vn@users.noreply.github.com>
2025-03-15 13:35:54 +0700

    yup validate for password

--- src/utils/rules.ts
+++ src/utils/rules.ts
@@ -75,7 +75,12 @@ export const schema = yup.object({
     .required('Email là bắt buộc') //
     .email('Email không đúng định dạng')
     .max(160, 'Độ dài từ 5 - 160 ký tự')
-    .min(5, 'Độ dài từ 5 - 160 ký tự')
+    .min(5, 'Độ dài từ 5 - 160 ký tự'),
+  password: yup
+    .string() //
+    .required('Password là bắt buộc')
+    .max(160, 'Độ dài từ 6 - 160 ký tự')
+    .min(6, 'Độ dài từ 6 - 160 ký tự')
 })
 
-export type Schema = yup.InferType<typeof schema>
+//export type Schema = yup.InferType<typeof schema>
