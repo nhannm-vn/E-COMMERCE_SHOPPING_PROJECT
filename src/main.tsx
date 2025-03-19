5b462400e9b2c9c1630379889ebf7fa6e7343ed0
Nhan Nguyen <153412098+nhannm-vn@users.noreply.github.com>
2025-03-20 05:58:47 +0700

    disable focus window refetch in react-query

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,13 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 //BrowserRouter: bọc app vào để chia route
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false
+    }
+  }
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
