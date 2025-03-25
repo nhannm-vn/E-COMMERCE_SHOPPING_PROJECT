8b92a44d2187fdab7027fbadd679b5ba27aa4285
Nhan Nguyen <153412098+nhannm-vn@users.noreply.github.com>
2025-03-25 09:55:21 +0700

    setup flow for interface

--- src/types/auth.type.ts
+++ src/types/auth.type.ts
@@ -1,2 +1,10 @@
 // Chứa những file hoặc interface liên quan đến việc login hoặc register
 // nghĩa là liên quan đến phần authentication
+
+import { ResponseApi } from './utils.type'
+
+export type Auth = ResponseApi<{
+  access_token: string
+  expires: string
+  user: User
+}>
