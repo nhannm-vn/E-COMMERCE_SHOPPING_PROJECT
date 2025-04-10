fac5b70ca7275069fef23a1e8e0b4c06cd0961fe
Nhan Nguyen <153412098+nhannm-vn@users.noreply.github.com>
2025-04-10 16:38:49 +0700

    import toastConTainer in app

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,5 +1,6 @@
 // file chuyên dùng để config axios
 import axios, { AxiosError, AxiosInstance } from 'axios'
+import HttpStatusCode from '../constants/httpStatusCode.enum'
 
 //keeptrying
 class Http {
@@ -21,6 +22,11 @@ class Http {
         // Các lỗi khác thì sẽ là object theo back-end quy định
         // Tuy nhiên đây là trường hợp các lỗi còn lại thì nó sẽ chỉ có message do axios render
         // Nghĩa là lỗi này sẽ khác với lỗi mã 422
+        if (error.response?.status !== HttpStatusCode.UnprocessableEntity) {
+          const data: any | undefined = error.response?.data
+          // Nếu mà trong data không có message thì hãy hãy message ở ngoài error luôn đi
+          const message = data.message || error.message
+        }
         return Promise.reject(error)
       }
     )
