b183e81c5a77e7d051cc15456398a840b26ac1cd
Nhan Nguyen <153412098+nhannm-vn@users.noreply.github.com>
2025-04-10 16:51:32 +0700

    create func call api login

--- src/apis/auth.api.ts
+++ src/apis/auth.api.ts
@@ -6,4 +6,9 @@ import http from '../utils/http'
 export const registerAccount = (body: {
   email: string //
   password: string
-}) => http.post<AuthResponse>('registerr', body)
+}) => http.post<AuthResponse>('register', body)
+
+export const login = (body: {
+  email: string //
+  password: string
+}) => http.post<AuthResponse>('login', body)
