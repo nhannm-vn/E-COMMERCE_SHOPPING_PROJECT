b1df2008e8af2431428ed68b4d3a1714d046e620
Nhan Nguyen <153412098+nhannm-vn@users.noreply.github.com>
2025-04-17 08:24:39 +0700

    update done Provider

--- src/contexts/app.context.tsx
+++ src/contexts/app.context.tsx
@@ -1,19 +1,36 @@
 import { createContext, useState } from 'react'
 import { getAccessTokenFromLS } from '../utils/auth'
 
+// Định nghĩa context lưu dữ liệu kiểu gì
 interface AppContextInterface {
   isAuthenticated: boolean
-  setIsAuthenticated: () => void
+  setIsAuthenticated: React.Dispatch<React.SetStateAction<boolean>>
 }
 
+// initialState giúp coi ban đầu sẽ lưu gì
 const initialAppContext: AppContextInterface = {
   // Nếu lấy ra được access_token thì sẽ là true, còn là '' thì ép kiểu về false
   isAuthenticated: Boolean(getAccessTokenFromLS()),
-  setIsAuthenticated: () => void
+  setIsAuthenticated: () => null
 }
 
-export const AppContext = createContext<AppContextInterface>()
+// <AppContextInterface>: nó quản lí kiểu dữ liệu là gì
+// initialAppContext: chúng ta dùng createContext thì bắt buộc truyền giá trị khởi tạo này
+// khi chúng ta không truyền vào Provider giá trị nào thì nó sẽ xài
+export const AppContext = createContext<AppContextInterface>(initialAppContext)
 
-export const AppProvider = ({children}: {children: React.ReactNode}) => {
+export const AppProvider = ({ children }: { children: React.ReactNode }) => {
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(initialAppContext.isAuthenticated)
+
+  // Nếu không có value thì nó sẽ lấy inititalAppContext
+  return (
+    <AppContext.Provider
+      value={{
+        isAuthenticated, //
+        setIsAuthenticated
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  )
 }
