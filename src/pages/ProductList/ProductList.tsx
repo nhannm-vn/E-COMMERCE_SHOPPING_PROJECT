3788f1e50323f1fbcc0c2604d57dd1d6d82bb01d
Nhan Nguyen <153412098+nhannm-vn@users.noreply.github.com>
2025-04-25 09:19:31 +0700

    generate list product

--- src/pages/ProductList/ProductList.tsx
+++ src/pages/ProductList/ProductList.tsx
@@ -1,4 +1,5 @@
 import AsideFilter from './AsideFilter'
+import Product from './Product/Product'
 import SortProductList from './SortProductList'
 
 function ProductList() {
@@ -12,7 +13,15 @@ function ProductList() {
           <div className='col-span-9'>
             <SortProductList />
             {/* chia theo break-point */}
-            <div className='mt-6 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-3'></div>
+            <div className='mt-6 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-3'>
+              {Array(30)
+                .fill(0)
+                .map((_, index) => (
+                  <div className='col-span-1' key={index}>
+                    <Product />
+                  </div>
+                ))}
+            </div>
           </div>
         </div>
       </div>
