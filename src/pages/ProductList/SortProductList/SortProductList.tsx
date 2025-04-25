150e9f8ef0c37cd890f588dcaede63982d4cef4b
Nhan Nguyen <153412098+nhannm-vn@users.noreply.github.com>
2025-04-25 15:45:21 +0700

    code SortProductList

--- src/pages/ProductList/SortProductList/SortProductList.tsx
+++ src/pages/ProductList/SortProductList/SortProductList.tsx
@@ -6,6 +6,12 @@ function SortProductList() {
         <button className='h-8 px-4 capitalize bg-orange text-white text-sm hover:bg-orange/80 text-center'>
           Phổ biến
         </button>
+        <button className='h-8 px-4 capitalize bg-white text-black text-sm hover:bg-slate-100 text-center'>
+          Mới nhất
+        </button>
+        <button className='h-8 px-4 capitalize bg-white text-black text-sm hover:bg-slate-100 text-center'>
+          Bán chạy
+        </button>
       </div>
     </div>
   )
