8e1caa02b816888bbada20801b54fe2fdb178b0e
Nhan Nguyen <153412098+nhannm-vn@users.noreply.github.com>
2025-04-25 09:00:28 +0700

    divide for AsideFilter

--- src/pages/ProductList/ProductList.tsx
+++ src/pages/ProductList/ProductList.tsx
@@ -1,5 +1,17 @@
+import AsideFilter from './AsideFilter'
+
 function ProductList() {
-  return <div>ProductList</div>
+  return (
+    <div className='border-[2px] border-red-500 bg-gray-200 py-6'>
+      <div className='container border-[2px] border-green-600'>
+        <div className='grid grid-cols-12 gap-6'>
+          <div className='col-span-3'>
+            <AsideFilter />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default ProductList
