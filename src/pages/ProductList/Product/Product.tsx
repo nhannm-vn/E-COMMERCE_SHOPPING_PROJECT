1138b14b20f652192b9e362424377a0859f87007
Nhan Nguyen <153412098+nhannm-vn@users.noreply.github.com>
2025-04-25 17:43:00 +0700

    code component Product

--- src/pages/ProductList/Product/Product.tsx
+++ src/pages/ProductList/Product/Product.tsx
@@ -1,11 +1,17 @@
 import { Link } from 'react-router-dom'
 
 // Ở mỗi item thì sẽ là một thẻ link khác nhau
-
+// Xác định thời gian chuyển đổi đó là 100ms.
+// Phải đi kèm với transition để hoạt động.
 function Product() {
   return (
     <Link to='/' className='border-[2px] border-yellow-400'>
-      <div className=''></div>
+      <div
+        className='bg-white shadow rounded-sm hover:translate-y-[-0.0625rem] hover:shadow-md duration-100
+      transition-transform'
+      >
+        Product
+      </div>
     </Link>
   )
 }
