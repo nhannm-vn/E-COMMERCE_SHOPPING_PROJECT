0b9b8005aba1c694d45ce49eba261206e8a14f0c
Nhan Nguyen <153412098+nhannm-vn@users.noreply.github.com>
2025-04-25 23:31:17 +0700

    code img for Product

--- src/pages/ProductList/Product/Product.tsx
+++ src/pages/ProductList/Product/Product.tsx
@@ -5,13 +5,19 @@ import { Link } from 'react-router-dom'
 // Phải đi kèm với transition để hoạt động.
 function Product() {
   return (
-    <Link to='/' className='border-[2px] border-yellow-400'>
+    <Link to='/'>
       <div
         className='bg-white shadow rounded-sm hover:translate-y-[-0.0625rem] hover:shadow-md duration-100
       transition-transform'
       >
         {/* Mẹo pt-[100%] và cho position img để tấm hình thành hình vuông*/}
-        <div className='w-full pt-[100%] relative'></div>
+        <div className='w-full pt-[100%] relative'>
+          <img
+            className='absolute top-0 left-0 bg-white w-full h-full object-cover'
+            src='https://down-vn.img.susercontent.com/file/vn-11134207-7ras8-m601pzsbaw1k32_tn.webp'
+            alt=''
+          />
+        </div>
       </div>
     </Link>
   )
