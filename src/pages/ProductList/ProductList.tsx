caf13df9df0e3bda85ca9bc3271aac7b156bedff
Nhan Nguyen <153412098+nhannm-vn@users.noreply.github.com>
2025-04-25 16:16:12 +0700

    update UI

--- src/pages/ProductList/ProductList.tsx
+++ src/pages/ProductList/ProductList.tsx
@@ -4,7 +4,7 @@ import SortProductList from './SortProductList'
 
 function ProductList() {
   return (
-    <div className='border-[2px] border-red-500 bg-gray-200 py-6'>
+    <div className='bg-gray-200 py-6'>
       <div className='container border-[2px] border-green-600'>
         <div className='grid grid-cols-12 gap-6 border-[2px] border-yellow-300'>
           <div className='col-span-3'>
