1e5cb9a4b6e65fdaf2f72e8820ea87491f6910aa
Nhan Nguyen <153412098+nhannm-vn@users.noreply.github.com>
2025-04-25 09:55:12 +0700

    continute code AsideFilter

--- src/pages/ProductList/AsideFilter/AsideFilter.tsx
+++ src/pages/ProductList/AsideFilter/AsideFilter.tsx
@@ -4,6 +4,7 @@ import path from '../../../constants/path'
 function AsideFilter() {
   return (
     <div className='py-4 border-[2px] border-purple-900'>
+      {/*  */}
       <Link to={path.home} className='flex items-center font-bold'>
         <svg viewBox='0 0 12 10' className='mr-3 h-4 w-3 fill-current'>
           <g fillRule='evenodd' stroke='none' strokeWidth={1}>
@@ -20,6 +21,44 @@ function AsideFilter() {
         </svg>
         Tất Cả Danh Mục
       </Link>
+      <div className='bg-gray-300 h-[1px] my-4' />
+      <ul>
+        <li className='py-2 pl-2'>
+          <Link to={path.home} className='relative px-2 text-orange font-semibold'>
+            <svg viewBox='0 0 4 7' className='fill-orange h-2 w-2 absolute top-1 left-[-10px]'>
+              <polygon points='4 3.5 0 0 0 7' />
+            </svg>
+            Thời Trang Nam
+          </Link>
+        </li>
+        <li className='py-2 pl-2'>
+          <Link to={path.home} className='relative px-2 '>
+            Điện Thoại
+          </Link>
+        </li>
+      </ul>
+      {/*  */}
+      <Link to={path.home} className='flex items-center font-bold mt-4 uppercase'>
+        <svg
+          enableBackground='new 0 0 15 15'
+          viewBox='0 0 15 15'
+          x={0}
+          y={0}
+          className='w-3 h-4 fill-current stroke-current mr-3'
+        >
+          <g>
+            <polyline
+              fill='none'
+              points='5.5 13.2 5.5 5.8 1.5 1.2 13.5 1.2 9.5 5.8 9.5 10.2'
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              strokeMiterlimit={10}
+            />
+          </g>
+        </svg>
+        Bộ lọc tìm kiếm
+      </Link>
+      <div className='bg-gray-300 h-[1px] my-4' />
     </div>
   )
 }
