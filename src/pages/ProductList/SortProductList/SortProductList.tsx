022092973d844a5dae7c1cb3737f4c65f9e3e6dc
Nhan Nguyen <153412098+nhannm-vn@users.noreply.github.com>
2025-04-25 16:12:43 +0700

    done UI SortProductList

--- src/pages/ProductList/SortProductList/SortProductList.tsx
+++ src/pages/ProductList/SortProductList/SortProductList.tsx
@@ -12,13 +12,48 @@ function SortProductList() {
         <button className='h-8 px-4 capitalize bg-white text-black text-sm hover:bg-slate-100 text-center'>
           Bán chạy
         </button>
-        <select className='h-8 px-4 capitalize bg-white text-black text-sm hover:bg-slate-100 text-left'>
+        <select
+          className='h-8 px-4 capitalize bg-white text-black text-sm hover:bg-slate-100 text-left outline-none'
+          value=''
+        >
           <option value='' disabled>
             Giá
           </option>
           <option value='price:asc'>Giá thấp đến cao</option>
           <option value='price:desc'>Giá cao đến thấp</option>
         </select>
+      </div>
+      <div className='flex items-center'>
+        <div>
+          <span className='text-orange'>1</span>
+          <span>/2</span>
+        </div>
+        <div className='ml-2'>
+          <button className='px-3 h-8 rounded-tl-sm rounded-bl-sm bg-white/60 hover:bg-slate-100 cursor-not-allowed shadow'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              fill='none'
+              viewBox='0 0 24 24'
+              strokeWidth={1.5}
+              stroke='currentColor'
+              className='w-3 h-3'
+            >
+              <path strokeLinecap='round' strokeLinejoin='round' d='M15.75 19.5 8.25 12l7.5-7.5' />
+            </svg>
+          </button>
+          <button className='px-3 h-8 rounded-tr-sm rounded-br-sm bg-white hover:bg-slate-100 shadow'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              fill='none'
+              viewBox='0 0 24 24'
+              strokeWidth={1.5}
+              stroke='currentColor'
+              className='w-3 h-3'
+            >
+              <path strokeLinecap='round' strokeLinejoin='round' d='m8.25 4.5 7.5 7.5-7.5 7.5' />
+            </svg>
+          </button>
+        </div>
       </div>
     </div>
   )
