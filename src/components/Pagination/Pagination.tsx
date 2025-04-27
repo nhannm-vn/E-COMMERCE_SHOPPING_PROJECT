de85d47533f9ccd44f0322213dbcaad5baf63211
Nhan Nguyen <153412098+nhannm-vn@users.noreply.github.com>
2025-04-27 18:48:48 +0700

    update flow pagination

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -42,15 +42,22 @@ function Pagination({ page, setPage, pageSize }: Props) {
       .map((_, index) => {
         const pageNumber = index + 1
         // Trường hợp dot ở sau page hiển thị các dấu ...
-        // _Thứ nhất currentPage <= 5 do business rule sao cho cách 2 đơn vị
-        // _Thứ hai những pageNumber render map ra phải cách thằng đang đứng 2 đơn vị
+        // _Thứ nhất page <= 5 do business rule sao cho cách 2 đơn vị
+        // _Thứ hai những pageNumber render map ra phải cách thằng đang đứng(page) 2 đơn vị
         // _Thứ ba những pageNumber render map ra phải nhỏ hơn thằng cuối 2 đơn vị
         //=> thì sẽ không render ra pageNumber đó mà render ra (button ...) một lần sau đó còn lại thì bỏ qua
         if (page <= RANGE * 2 + 1 && pageNumber > page + RANGE && pageNumber < pageSize - RANGE + 1) {
           return renderDotAfter(index)
-        } else if (page > RANGE * 2 + 1 && page < pageSize - RANGE * 2) {
-          if (pageNumber < page - RANGE && pageNumber > RANGE) {
+        }
+        // Trường hợp có dấu ... ở trước và cả sau page
+        // _Thứ nhất page > 5 lúc này sẽ khác hoàn toàn trường hợp 1
+        // _Thứ hai page < pageSize - RANGE * 2 nghĩa là page nó chắc chắc không vượt qua 15 thì
+        // mới thỏa hiển thị ... khi đứng ở 15 còn đứng 16 thì hiển thị đủ 17 18 19 20 nên không bị ẩn
+        else if (page > RANGE * 2 + 1 && page < pageSize - RANGE * 2) {
+          // Những thằng nào > 2 và đồng thời phải cách thằng page 2 thì cho ra ...
+          if (pageNumber > RANGE && pageNumber < page - RANGE) {
             return renderDotBefore(index)
+            // Những thằng nào cách page 2 và cách 20 2 đơn vị thì cho ra ...
           } else if (pageNumber > page + RANGE && pageNumber < pageSize - RANGE + 1) {
             return renderDotAfter(index)
           }
