f06efbf7e4b6b9c43511ba8ecbce0144e45c0a44
Nhan Nguyen <153412098+nhannm-vn@users.noreply.github.com>
2025-05-07 17:05:05 +0700

    show sold in ProductDetail

--- src/pages/ProductDetail/ProductDetail.tsx
+++ src/pages/ProductDetail/ProductDetail.tsx
@@ -2,6 +2,7 @@ import { useQuery } from '@tanstack/react-query'
 import { useParams } from 'react-router-dom'
 import productApi from '../../apis/product.api'
 import ProductRating from '../../components/ProductRating'
+import { formatNumberToSocialStyle } from '../../utils/utils'
 
 function ProductDetail() {
   const { id } = useParams()
@@ -78,6 +79,7 @@ function ProductDetail() {
               <div className='border-black-500 col-span-7 border-[1px]'>
                 <h1 className='text-xl font-medium capitalize'>{product.name}</h1>
                 <div className='mt-8 flex items-center'>
+                  {/* Rating */}
                   <div className='flex items-center'>
                     <span className='mr-1 border border-b border-b-orange border-l-transparent border-r-transparent border-t-transparent text-orange'>
                       {product.rating}
@@ -87,6 +89,14 @@ function ProductDetail() {
                       activeClassname='fill-orange text-orange h-4 w-4' //
                       nonActiveClassname='fill-gray-300 text-gray-300 h-4 w-4'
                     />
+                  </div>
+                  <div className='mx-4 h-4 w-[1px] bg-gray-300'></div>
+                  {/* Sold */}
+                  <div>
+                    <span className='mr-1 border border-b border-b-black border-l-transparent border-r-transparent border-t-transparent text-black'>
+                      {formatNumberToSocialStyle(product.sold)}
+                    </span>
+                    <span className='ml-1 text-gray-500'>Đã bán</span>
                   </div>
                 </div>
               </div>
