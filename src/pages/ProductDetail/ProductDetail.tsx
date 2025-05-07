098f18f106658350adb5c0b75c29eefa70609b4e
Nhan Nguyen <153412098+nhannm-vn@users.noreply.github.com>
2025-05-07 10:15:04 +0700

    fetch data in ProductDetail

--- src/pages/ProductDetail/ProductDetail.tsx
+++ src/pages/ProductDetail/ProductDetail.tsx
@@ -1,4 +1,17 @@
+import { useQuery } from '@tanstack/react-query'
+import { useParams } from 'react-router-dom'
+import productApi from '../../apis/product.api'
+
 function ProductDetail() {
+  const { id } = useParams()
+  const { data: productDetailData } = useQuery({
+    // Điền cái id để khi id thay đổi thì dữ liệu sẽ fetch lại
+    queryKey: ['product', id],
+    // Phải as string vì mình biết id lúc nào cũng có
+    queryFn: () => productApi.getProductDetail(id as string)
+  })
+  // Lấy dữ liệu ra
+  const product = productDetailData?.data.data
   return <div>ProductDetail</div>
 }
 
