4e14d6a8c1eb56049543b57a9fd1a54bc8896c18
Nhan Nguyen <153412098+nhannm-vn@users.noreply.github.com>
2025-05-09 10:37:28 +0700

    code button + and input number

--- src/pages/ProductDetail/ProductDetail.tsx
+++ src/pages/ProductDetail/ProductDetail.tsx
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom'
 import productApi from '../../apis/product.api'
 import ProductRating from '../../components/ProductRating'
 import { formatCurrency, formatNumberToSocialStyle, rateSale } from '../../utils/utils'
+import InputNumber from '../../components/InputNumber'
 
 function ProductDetail() {
   const { id } = useParams()
@@ -142,6 +143,24 @@ function ProductDetail() {
                         className='size-4'
                       >
                         <path strokeLinecap='round' strokeLinejoin='round' d='M5 12h14' />
+                      </svg>
+                    </button>
+                    <InputNumber
+                      value={1}
+                      className=''
+                      classNameError='hidden'
+                      classNameInput='h-8 w-14 border-t border-b border-gray-300 p-1 text-center outline-none'
+                    />
+                    <button className='flex h-8 w-8 items-center justify-center rounded-r-sm border border-gray-300 text-gray-600'>
+                      <svg
+                        xmlns='http://www.w3.org/2000/svg'
+                        fill='none'
+                        viewBox='0 0 24 24'
+                        strokeWidth={1.5}
+                        stroke='currentColor'
+                        className='size-6'
+                      >
+                        <path strokeLinecap='round' strokeLinejoin='round' d='M12 4.5v15m7.5-7.5h-15' />
                       </svg>
                     </button>
                   </div>
