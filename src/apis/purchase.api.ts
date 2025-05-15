72349d1bf3a7d99fa6c8d25b6bf552831ccf4cb3
Nhan Nguyen <153412098+nhannm-vn@users.noreply.github.com>
2025-05-15 18:23:18 +0700

    done code interface and api purchase and constants purchase status

--- src/apis/purchase.api.ts
+++ src/apis/purchase.api.ts
@@ -5,12 +5,16 @@ import http from '../utils/http'
 const URL = 'purchases'
 
 const purchaseApi = {
+  // Thêm một đơn hàng
   addToCart(body: { product_id: string; buy_count: number }) {
     return http.post<SuccessResponse<Purchase>>(`${URL}/add-to-cart`, body)
   },
+  // Xem thông tin đơn hàng
   getPurchases(params: { status: PurchaseListStatus }) {
     return http.get<SuccessResponse<Purchase[]>>(`${URL}`, {
       params
     })
   }
 }
+
+export default purchaseApi
--- src/types/purchase.type.ts
+++ src/types/purchase.type.ts
@@ -2,11 +2,13 @@ import { Product } from './product.type'
 
 // Status này nghĩa là  hiển thị trạng thái của
 //từng đơn hàng. Nếu như vậy thì không bao gồm số 0 nghĩa là lấy tất cả trạng thái
+//nghĩa là dùng cho hiển thị trạng thái
 
 export type PurchaseStatus = -1 | 1 | 2 | 3 | 4 | 5
 
 // Status này là con số sẽ gửi kèm lên để lấy phân loại đơn hàng nên sẽ
 //có các số khác kèm theo số 0 lấy tất cả trạng thái
+//nghĩa là dùng cho gửi lên
 export type PurchaseListStatus = PurchaseStatus | 0
 
 export interface Purchase {
