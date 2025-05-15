a8af26b49ce3732859326edcd5cea9c89abd6915
Nhan Nguyen <153412098+nhannm-vn@users.noreply.github.com>
2025-05-15 18:14:32 +0700

    update api and interface purchase

---
+++ src/types/purchase.type.ts
@@ -0,0 +1,22 @@
+import { Product } from './product.type'
+
+// Status này nghĩa là  hiển thị trạng thái của
+//từng đơn hàng. Nếu như vậy thì không bao gồm số 0 nghĩa là lấy tất cả trạng thái
+
+export type PurchaseStatus = -1 | 1 | 2 | 3 | 4 | 5
+
+// Status này là con số sẽ gửi kèm lên để lấy phân loại đơn hàng nên sẽ
+//có các số khác kèm theo số 0 lấy tất cả trạng thái
+export type PurchaseListStatus = PurchaseStatus | 0
+
+export interface Purchase {
+  _id: string
+  buy_count: number
+  price: number
+  price_before_discount: number
+  status: PurchaseStatus
+  user: string
+  product: Product
+  createdAt: string
+  updatedAt: string
+}
