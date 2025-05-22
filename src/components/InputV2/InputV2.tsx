0280a4a4531b349630041364c799b2a61f996853
Nhan Nguyen <153412098+nhannm-vn@users.noreply.github.com>
2025-05-22 19:08:11 +0700

    customer Input and InputNumber to InputV2 by useController

---
+++ src/components/InputV2/InputV2.tsx
@@ -0,0 +1,8 @@
+// Đối với phiên bản này mình chỉ dùng được với react-hook-form thôi
+//đối với thằng này sẽ dùng useController
+
+function InputV2() {
+  return <div>InputV2</div>
+}
+
+export default InputV2
