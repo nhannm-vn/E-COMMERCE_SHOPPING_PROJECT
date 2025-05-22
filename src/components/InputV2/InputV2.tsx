b889c22fec028164e5e3d3abc7170f351bfb7d09
Nhan Nguyen <153412098+nhannm-vn@users.noreply.github.com>
2025-05-22 19:11:50 +0700

    start code component InputV2

--- src/components/InputV2/InputV2.tsx
+++ src/components/InputV2/InputV2.tsx
@@ -1,8 +1,51 @@
 // Đối với phiên bản này mình chỉ dùng được với react-hook-form thôi
 //đối với thằng này sẽ dùng useController
 
-function InputV2() {
-  return <div>InputV2</div>
+import { forwardRef, InputHTMLAttributes, useState } from 'react'
+
+// Khai báo interface cho prop
+// **Nhờ extend mà mình có thể không cần ĐỊNH NGHĨA hết các thuộc tính vd như placeholder
+export interface InputNumberProps extends InputHTMLAttributes<HTMLInputElement> {
+  // type: React.HTMLInputTypeAttribute
+  errrorMessage?: string
+  classNameInput?: string
+  classNameError?: string
 }
 
+const InputV2 = forwardRef<HTMLInputElement, InputNumberProps>(function InputNumberInner(
+  {
+    errrorMessage,
+    className,
+    classNameInput = 'p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm focus:shadow-md',
+    classNameError = 'mt-1 text-red-600 min-h-[1.3rem] text-sm',
+    onChange,
+    value = '',
+    ...rest
+  },
+  ref
+) {
+  const [localValue, setLocalValue] = useState<string>(value as string)
+  // Nghiã là khi người dùng gõ số thì onChange nó mới chạy
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target
+    if (/^\d+$/.test(value) || value === '') {
+      // Thuc thi onChange callback từ bên ngoài truyền vào props
+      if (onChange) {
+        onChange(event)
+      }
+      // Cập nhật localValue
+      setLocalValue(value)
+    }
+  }
+  return (
+    <div className={className}>
+      <input className={classNameInput} {...rest} onChange={handleChange} value={value || localValue} ref={ref} />
+      {/* min-h-[1rem]: giúp luôn có chiều cao kể cả không có lỗi */}
+      <div className={classNameError}>{errrorMessage}</div>
+    </div>
+  )
+})
+
 export default InputV2
+
+// Component này chuyên dùng riêng cho handler số xài đến onChangeEvent. Nên không có xài lại Input được do có register
