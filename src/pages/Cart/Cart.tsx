6213effeb1506f3368ea6f72af0d3d79a20a63ba
Nhan Nguyen <153412098+nhannm-vn@users.noreply.github.com>
2025-06-01 15:27:08 +0700

    done code header cart

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -13,9 +13,39 @@ function Cart() {
     // enabled: isAuthenticated
   })
 
+  // Móc data ra
   const purchasesInCart = purchasesInCartData?.data.data
 
-  return <div>Cart</div>
+  return (
+    <div className='bg-neutral-100 py-16'>
+      <div className='container'>
+        {/* Khi thu nhỏ thì cho một thanh nằm ngang */}
+        <div className='overflow-auto'>
+          <div className='min-w-[1000px]'>
+            {/* Header */}
+            <div className='grid grid-cols-12 rounded-sm bg-white px-9 py-5 text-sm capitalize text-gray-500 shadow'>
+              <div className='col-span-6'>
+                <div className='flex items-center'>
+                  <div className='flex flex-shrink-0 items-center justify-center pr-3'>
+                    <input type='checkbox' className='h-5 w-5 accent-orange' />
+                  </div>
+                  <div className='flex-grow text-black'>Sản phẩm</div>
+                </div>
+              </div>
+              <div className='col-span-6'>
+                <div className='grid grid-cols-5 text-center'>
+                  <div className='col-span-2'>Đơn giá</div>
+                  <div className='col-span-1'>Số lượng</div>
+                  <div className='col-span-1'>Số tiền</div>
+                  <div className='col-span-1'>Thao tác</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Cart
