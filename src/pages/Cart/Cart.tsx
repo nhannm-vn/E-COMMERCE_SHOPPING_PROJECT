1febf5d334527633e22da3a3a9f829c4d33b76ba
Nhan Nguyen <153412098+nhannm-vn@users.noreply.github.com>
2025-06-01 15:01:04 +0700

    call api purchase in component Cart

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,4 +1,20 @@
+import { useQuery } from '@tanstack/react-query'
+import purchaseApi from '../../apis/purchase.api'
+import { purchasesStatus } from '../../constants/purchase'
+
 function Cart() {
+  // Gọi api purchase list
+  const { data: purchasesInCartData } = useQuery({
+    queryKey: ['purchases', { status: purchasesStatus.inCart }],
+    queryFn: () => purchaseApi.getPurchases({ status: purchasesStatus.inCart })
+    // Khi chưa có login vào thì đừng gọi api của purchase
+    // **bên này thì không cần do biết là nếu chưa login thì
+    // sẽ không vào được bên trang này
+    // enabled: isAuthenticated
+  })
+
+  const purchasesInCart = purchasesInCartData?.data.data
+
   return <div>Cart</div>
 }
 
