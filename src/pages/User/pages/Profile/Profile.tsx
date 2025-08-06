f97e2571f2d2cca6596c53943f246395c7a8336c
Nhan Nguyen <153412098+nhannm-vn@users.noreply.github.com>
2025-08-06 23:59:57 +0700

    fix import in useRoute

--- src/pages/User/pages/Profile/Profile.tsx
+++ src/pages/User/pages/Profile/Profile.tsx
@@ -1,3 +1,7 @@
 export default function Profile() {
-  return <div>Profile</div>
+  return (
+    <div className='rounded-sm bg-white px-7 pb-20 shadow'>
+      <div className='border-b border-b-gray-200'></div>
+    </div>
+  )
 }
