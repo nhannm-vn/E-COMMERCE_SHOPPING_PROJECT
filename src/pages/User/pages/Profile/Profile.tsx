2d9cf8548cefd4eac08ddaa67519ff8cfe8bcc1f
Nhan Nguyen <153412098+nhannm-vn@users.noreply.github.com>
2025-08-07 10:05:10 +0700

    complete UI page Profile

--- src/pages/User/pages/Profile/Profile.tsx
+++ src/pages/User/pages/Profile/Profile.tsx
@@ -2,40 +2,40 @@ import Input from '../../../../components/Input'
 
 export default function Profile() {
   return (
-    <div className='rounded-sm bg-white px-7 pb-20 shadow'>
+    <div className='rounded-sm bg-white px-2 pb-10 shadow md:px-7 md:pb-20'>
       <div className='border-b border-b-gray-200 py-6'>
         <h1 className='text-lg font-medium capitalize text-gray-900'>Hồ sơ của tôi</h1>
         <div className='mt-1 text-sm text-gray-700'>Quản lý thông tin hồ sơ để bảo mật tài khoản</div>
       </div>
-      <div className='mt-8 flex flex-col-reverse md:flex-grow md:items-start'>
-        <form className='mt-6 w-[60%] flex-grow pr-12 md:mt-0'>
-          <div className='flex flex-wrap'>
-            <div className='w-[20%] truncate pt-3 text-right capitalize'>Email</div>
-            <div className='w-[80%] pl-5'>
+      <div className='mt-8 flex max-lg:flex-col-reverse md:flex-grow md:items-start'>
+        <form className='mt-6 flex-grow md:mt-0 md:pr-14'>
+          <div className='flex flex-col flex-wrap sm:flex-row'>
+            <div className='truncate pt-3 capitalize sm:w-[20%] sm:text-right'>Email</div>
+            <div className='sm:w-[80%] sm:pl-5'>
               <div className='pt-3 text-gray-700'>nhan*********@gmail.com</div>
             </div>
           </div>
-          <div className='mt-2 flex flex-wrap'>
-            <div className='w-[20%] truncate pt-3 text-right capitalize'>Tên</div>
-            <div className='w-[80%] pl-5'>
+          <div className='mt-2 flex flex-col flex-wrap sm:flex-row'>
+            <div className='truncate pt-3 capitalize sm:w-[20%] sm:text-right'>Tên</div>
+            <div className='sm:w-[80%] sm:pl-5'>
               <Input classNameInput='w-full rounded-sm border py-2 border-gray-300 px-3 outline-none focus:border-gray-500 focus:shadow-sm' />
             </div>
           </div>
-          <div className='mt-2 flex flex-wrap'>
-            <div className='w-[20%] truncate pt-3 text-right capitalize'>Số điện thoại</div>
-            <div className='w-[80%] pl-5'>
+          <div className='mt-2 flex flex-col flex-wrap sm:flex-row'>
+            <div className='truncate pt-3 capitalize sm:w-[20%] sm:text-right'>Số điện thoại</div>
+            <div className='sm:w-[80%] sm:pl-5'>
               <Input classNameInput='w-full rounded-sm border py-2 border-gray-300 px-3 outline-none focus:border-gray-500 focus:shadow-sm' />
             </div>
           </div>
-          <div className='mt-2 flex flex-wrap'>
-            <div className='w-[20%] truncate pt-3 text-right capitalize'>Địa chỉ</div>
-            <div className='w-[80%] pl-5'>
+          <div className='mt-2 flex flex-col flex-wrap sm:flex-row'>
+            <div className='truncate pt-3 capitalize sm:w-[20%] sm:text-right'>Địa chỉ</div>
+            <div className='sm:w-[80%] sm:pl-5'>
               <Input classNameInput='w-full rounded-sm border py-2 border-gray-300 px-3 outline-none focus:border-gray-500 focus:shadow-sm' />
             </div>
           </div>
-          <div className='mt-2 flex flex-wrap'>
-            <div className='w-[20%] truncate pt-3 text-right capitalize'>Ngày sinh</div>
-            <div className='w-[80%] pl-5'>
+          <div className='mt-2 flex flex-col flex-wrap sm:flex-row'>
+            <div className='truncate pt-3 capitalize sm:w-[20%] sm:text-right'>Ngày sinh</div>
+            <div className='sm:w-[80%] sm:pl-5'>
               <div className='flex justify-between'>
                 <select className='h-10 w-[32%] rounded-sm border border-black/10 px-3'>
                   <option disabled>Ngày</option>
@@ -50,6 +50,25 @@ export default function Profile() {
             </div>
           </div>
         </form>
+        <div className='flex justify-center md:w-72 md:border-l md:border-l-gray-200'>
+          <div className='flex flex-col items-center'>
+            <div className='my-5 h-24 w-24'>
+              <img
+                className='h-full w-full rounded-full object-cover'
+                src='https://images-cdn.openxcell.com/wp-content/uploads/2024/07/25085005/reactjs-inner.svg'
+                alt=''
+              />
+            </div>
+            <input className='hidden' type='file' accept='.jpg,.jpeg,.png' />
+            <button className='flex h-10 items-center justify-end rounded-sm border bg-white px-6 text-sm capitalize text-gray-600 shadow-sm'>
+              Chọn ảnh
+            </button>
+            <div className='mt-3 text-gray-400'>
+              <div>Dung lượng file tối đa 1 MB</div>
+              <div>Định dạng JPEG, PNG</div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
