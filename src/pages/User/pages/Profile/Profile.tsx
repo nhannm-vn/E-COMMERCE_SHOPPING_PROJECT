7956190036787ea9c659d0d6c2e991e508ba2a2b
Nhan Nguyen <153412098+nhannm-vn@users.noreply.github.com>
2025-08-08 21:57:42 +0700

    divide component DateSelect and prepare

--- src/pages/User/pages/Profile/Profile.tsx
+++ src/pages/User/pages/Profile/Profile.tsx
@@ -7,6 +7,7 @@ import { Controller, useForm } from 'react-hook-form'
 import { yupResolver } from '@hookform/resolvers/yup'
 import InputNumber from '../../../../components/InputNumber'
 import { useEffect } from 'react'
+import DateSelect from '../../components/DateSelect'
 
 // Mình sẽ không sử dụng omit vì nếu trong tương lai nếu schema mình xài
 //omit thì nó sẽ bị lỗi giữ lại những cái không mong muốn
@@ -125,22 +126,7 @@ export default function Profile() {
               />
             </div>
           </div>
-          <div className='mt-2 flex flex-col flex-wrap sm:flex-row'>
-            <div className='truncate pt-3 capitalize sm:w-[20%] sm:text-right'>Ngày sinh</div>
-            <div className='sm:w-[80%] sm:pl-5'>
-              <div className='flex justify-between'>
-                <select className='h-10 w-[32%] rounded-sm border border-black/10 px-3'>
-                  <option disabled>Ngày</option>
-                </select>
-                <select className='h-10 w-[32%] rounded-sm border border-black/10 px-3'>
-                  <option disabled>Tháng</option>
-                </select>
-                <select className='h-10 w-[32%] rounded-sm border border-black/10 px-3'>
-                  <option disabled>Năm</option>
-                </select>
-              </div>
-            </div>
-          </div>
+          <DateSelect />
           {/* Button */}
           <div className='mt-2 flex flex-col flex-wrap sm:flex-row'>
             {/* Điều này giúp cho button có thể giữ nguyên được hiện trạng và không bị chạy đi
