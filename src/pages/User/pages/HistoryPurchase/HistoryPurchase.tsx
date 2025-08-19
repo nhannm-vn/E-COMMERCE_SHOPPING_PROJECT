b80f27cedc48e3f215fce18797be0dacc2e4cff0
Nhan Nguyen <153412098+nhannm-vn@users.noreply.github.com>
2025-08-19 20:33:31 +0700

    prepare code HistoryPurchase

--- src/pages/User/pages/HistoryPurchase/HistoryPurchase.tsx
+++ src/pages/User/pages/HistoryPurchase/HistoryPurchase.tsx
@@ -1,3 +1,29 @@
+import { createSearchParams, Link } from 'react-router-dom'
+import path from '../../../../constants/path'
+import { purchasesStatus } from '../../../../constants/purchase'
+import classNames from 'classnames'
+import useQueryParams from '../../../../hooks/useQueryParams'
+
 export default function HistoryPurchase() {
-  return <div>HistoryPurchase</div>
+  //Lấy param từ hook đã custom sẵn
+  const queryParams: {status?: string} = useQueryParams()
+  return (
+    <div>
+      <div className='sticky top-0 flex rounded-t-sm shadow-sm'>
+        <Link
+          to={{
+            pathname: path.historyPurchase,
+            search: createSearchParams({
+              status: String(purchasesStatus.all)
+            }).toString()
+          }}
+          className={classNames('flex flex-1 items-center justify-center border-b-2 bg-white py-4 text-center', {
+            'border-b-orange text-orange'
+          })}
+        >
+          Tất cả
+        </Link>
+      </div>
+    </div>
+  )
 }
